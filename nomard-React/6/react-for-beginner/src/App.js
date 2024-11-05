286891ff2b3b7aac0755743c9265291edbec666a
3boku <hyunseojung@clika.io>
2024-11-05 11:19:07 +0900

    6.0 6.1

--- nomard-React/6/react-for-beginner/src/App.js
+++ nomard-React/6/react-for-beginner/src/App.js
@@ -1,10 +1,18 @@
-import Button from "./button"
+import {useState, useEffect } from "react";
 
 function App() {
+    const [counter, setValue] = useState(0);
+    const onClick = () => setValue((prev) => prev + 1);
+    console.log("I run all the time");
+    const iRunOnlyOnce = () => {
+        console.log("I run only once");
+    };
+
+    useEffect(iRunOnlyOnce, []);
   return (
     <div className="App">
-      <h1>Welcome, ReactJS!</h1>
-        <Button text="Click Me" />
+        <h1>{counter}</h1>
+        <button onClick={onClick}>Click Me!</button>
     </div>
   );
 }
