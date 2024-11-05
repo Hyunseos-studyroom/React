d4a75da2fc2d3e96627fa672a9452518bc55e756
3boku <hyunseojung@clika.io>
2024-11-05 11:10:09 +0900

    init: 6

---
+++ nomard-React/6/react-for-beginner/src/App.js
@@ -0,0 +1,12 @@
+import Button from "./button"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome, ReactJS!</h1>
+        <Button text="Click Me" />
+    </div>
+  );
+}
+
+export default App;
