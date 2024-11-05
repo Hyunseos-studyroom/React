f802cbeb890a37e2ec01aac5ac2e21dff54301a7
3boku <hyunseojung@clika.io>
2024-11-05 11:37:39 +0900

    6.2

--- nomard-React/6/react-for-beginner/src/App.js
+++ nomard-React/6/react-for-beginner/src/App.js
@@ -2,15 +2,24 @@ import {useState, useEffect } from "react";
 
 function App() {
     const [counter, setValue] = useState(0);
+    const [keyword, setKeyword] = useState("");
     const onClick = () => setValue((prev) => prev + 1);
-    console.log("I run all the time");
-    const iRunOnlyOnce = () => {
-        console.log("I run only once");
-    };
-
-    useEffect(iRunOnlyOnce, []);
+    const onChange = (event) => setKeyword(event.target.value);
+    useEffect(() => {
+        console.log("I run only once.")
+    }, []);
+    useEffect(() => {
+        console.log("I run when 'keyword' changes.");
+    }, [keyword]);
+    useEffect(() => {
+        console.log("I run when 'counter' changes.");
+    }, [counter])
+    useEffect(() => {
+        console.log("I run when keyword & counter changes.")
+    }, [keyword, counter]);
   return (
     <div className="App">
+        <input value={keyword} onChange={onChange} type={"text"} placeholder={"Search here..."}/>
         <h1>{counter}</h1>
         <button onClick={onClick}>Click Me!</button>
     </div>
