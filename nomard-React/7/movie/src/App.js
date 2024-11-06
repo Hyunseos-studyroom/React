c3857f884ff5c77ce13492341673930625e7a608
3boku <hyunseojung@clika.io>
2024-11-06 17:29:38 +0900

    asdfasdf

--- nomard-React/7/movie/src/App.js
+++ nomard-React/7/movie/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+    )
+        .then((response) => response.json())
+        .then((json) => console.log(json))
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {loading ? <h1>Loading...</h1> : null}
     </div>
   );
 }
