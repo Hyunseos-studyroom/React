f19b49a0c64114a6a6da3dca17c9d2c53ab052b1
3boku <hyunseojung@clika.io>
2024-11-07 11:28:33 +0900

    7.4

--- nomard-React/7/movie/src/App.js
+++ nomard-React/7/movie/src/App.js
@@ -1,19 +1,40 @@
-import {useEffect, useState} from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
+  const url = "https://nomad-movies.nomadcoders.workers.dev/movies";
+  const getMovies = async () => {
+    const json = await (await fetch(url)).json();
+    setMovies(json);
+    setLoading(false);
+  };
   useEffect(() => {
-    fetch(
-        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-    )
-        .then((response) => response.json())
-        .then((json) => console.log(json))
+    getMovies();
   }, []);
+  console.log(movies);
   return (
-    <div className="App">
-      {loading ? <h1>Loading...</h1> : null}
-    </div>
+      <div className="App">
+        {loading ? (
+            <h1>loading...</h1>
+        ) : (
+            <div>
+              {movies.map((movie) => (
+                  <div key={movie.id}>
+                    <h2>{movie.original_title}</h2>
+                    <p>{movie.overview}</p>
+                    <img src={movie.poster_path} style={{width:"200px"}} />
+                    <ul>
+                      {movie.genre_ids.map((genre)=>(
+                          <li key={genre}>{genre}</li>
+                      ))}
+                    </ul> {/*장르*/}
+                    <hr />
+                  </div>
+              ))}
+            </div>
+        )}
+      </div>
   );
 }
 
